e7de7631e61051c386853059533be7d70d8c0ed1
lil L <57925562+leolopez10@users.noreply.github.com>
2020-05-04 12:06:14 -0500

    created api routes for authentication,  users, profiles, and posts

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('API Running'))
 
+// Defining Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 
 //Database Connection
 mongoose.connect((process.env.MONGODB_URI || process.env.DATABASE), {

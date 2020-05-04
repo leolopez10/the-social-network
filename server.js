ce4d143f75cbe96669fdbd3ae84b9dbcfdb30730
lil L <57925562+leolopez10@users.noreply.github.com>
2020-05-04 11:35:16 -0500

    set up databse

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('API Running'))
 
-const PORT = process.env.PORT || 5000;
 
+//Database Connection
+mongoose.connect((process.env.MONGODB_URI || process.env.DATABASE), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+    .then(() => console.log("====================DATABASE CONNECTED===================="))
+    .catch(error => console.log(error));
+
+//Server Listening
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));

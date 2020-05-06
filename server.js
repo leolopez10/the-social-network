79f637638298ee268c47db7ad65c5bdf165eb181
lil L <57925562+leolopez10@users.noreply.github.com>
2020-05-06 11:18:53 -0500

    created a react app, landing page

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Init Middleware
 app.use(express.json({ extended: false }))

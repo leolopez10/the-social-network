f850b609abd2d5d90a0b8596c441fcf54fcaa962
lil L <57925562+leolopez10@users.noreply.github.com>
2020-05-08 13:38:28 -0500

    created an authentication reducer and action to check for frontend authentication

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,12 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
+import PropTypes from 'prop-types'
 
-const Register = () => {
+
+const Register = ({ setAlert, register }) => { //setAlert is destructred from props (i.e. props.setAlert())
 
     const [formData, setFormData] = useState({
         name: '',
@@ -17,10 +22,14 @@ const Register = () => {
     const onSubmit = async event => {
         event.preventDefault();
         if (password !== password2) {
-            console.log('Passwords do match');
+            // console.log('Passwords do match');
+            setAlert('Passwords do not match', 'danger');
         } else {
             // console.log(formData);
-            console.log('Success')
+            // console.log('Success')
+            register({ name, email, password })
+
+            // Moved all this code to the actions folder
             // const newUser = {
             //     name,
             //     email,
@@ -45,61 +54,68 @@ const Register = () => {
 
 
 
-    return <Fragment>
-        <h1 className='large text-primary'>Sign Up</h1>
-        <p className='lead'><i className='fas fa-user'></i> Create Your Account</p>
-        <form className='form' onSubmit={event => onSubmit(event)}>
-            <div className='form-group'>
-                <input
-                    type='text'
-                    placeholder='Name'
-                    name='name'
-                    value={name}
-                    onChange={event => onChange(event)}
-                    required
-                />
-            </div>
-            <div className='form-group'>
-                <input
-                    type='email'
-                    placeholder='Email Address'
-                    name='email'
-                    value={email}
-                    onChange={event => onChange(event)}
-                    require
-                />
-                <small className='form-text'
-                >This site uses Gravatar so if you want a profile image, use a
+    return (
+        <Fragment>
+            <h1 className='large text-primary'>Sign Up</h1>
+            <p className='lead'><i className='fas fa-user'></i> Create Your Account</p>
+            <form className='form' onSubmit={event => onSubmit(event)}>
+                <div className='form-group'>
+                    <input
+                        type='text'
+                        placeholder='Name'
+                        name='name'
+                        value={name}
+                        onChange={event => onChange(event)}
+                        required
+                    />
+                </div>
+                <div className='form-group'>
+                    <input
+                        type='email'
+                        placeholder='Email Address'
+                        name='email'
+                        value={email}
+                        onChange={event => onChange(event)}
+                        required
+                    />
+                    <small className='form-text'
+                    >This site uses Gravatar so if you want a profile image, use a
             Gravatar email</small
-                >
-            </div>
-            <div className='form-group'>
-                <input
-                    type='password'
-                    placeholder='Password'
-                    name='password'
-                    value={password}
-                    onChange={event => onChange(event)}
-                    minLength='6'
-                />
-            </div>
-            <div className='form-group'>
-                <input
-                    type='password'
-                    placeholder='Confirm Password'
-                    name='password2'
-                    value={password2}
-                    onChange={event => onChange(event)}
-                    minLength='6'
-                />
-            </div>
-            <input type='submit' className='btn btn-primary' value='Register' />
-        </form>
-        <p className='my-1'>
-            Already have an account? <Link to='/login'>Sign In</Link>
-        </p>
+                    >
+                </div>
+                <div className='form-group'>
+                    <input
+                        type='password'
+                        placeholder='Password'
+                        name='password'
+                        value={password}
+                        onChange={event => onChange(event)}
+                        minLength='6'
+                    />
+                </div>
+                <div className='form-group'>
+                    <input
+                        type='password'
+                        placeholder='Confirm Password'
+                        name='password2'
+                        value={password2}
+                        onChange={event => onChange(event)}
+                        minLength='6'
+                    />
+                </div>
+                <input type='submit' className='btn btn-primary' value='Register' />
+            </form>
+            <p className='my-1'>
+                Already have an account? <Link to='/login'>Sign In</Link>
+            </p>
 
-    </Fragment>
-}
+        </Fragment>
+    );
+};
 
-export default Register
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setAlert, register })(Register);

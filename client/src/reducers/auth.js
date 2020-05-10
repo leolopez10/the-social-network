bc5444e3313720c0217f6beb6d893007f8cd50bc
lil L <57925562+leolopez10@users.noreply.github.com>
2020-05-10 13:40:29 -0500

    listed user experience and education and created ways to delete user experience, eduation, and account

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -6,13 +6,14 @@ import {
   AUTH_ERROR,
   LOGIN_FAIL,
   LOGOUT,
+  ACCOUNT_DELETED
 } from '../actions/types';
 
 const initialState = {
   token: localStorage.getItem('token'),
   isAuthenticated: null,
   loading: true,
-  user: null,
+  user: null
 };
 
 export default (state = initialState, action) => {
@@ -23,7 +24,7 @@ export default (state = initialState, action) => {
         ...state,
         isAuthenticated: true,
         loading: false,
-        user: payload,
+        user: payload
       };
     case REGISTER_SUCCESS:
     case LOGIN_SUCCESS:
@@ -32,18 +33,19 @@ export default (state = initialState, action) => {
         ...state,
         ...payload,
         isAuthenticated: true,
-        loading: false,
+        loading: false
       };
     case LOGIN_FAIL:
     case AUTH_ERROR:
     case REGISTER_FAIL:
     case LOGOUT:
+    case ACCOUNT_DELETED:
       localStorage.removeItem('token');
       return {
         ...state,
         token: null,
         isAuthenticated: false,
-        loading: false,
+        loading: false
       };
     default:
       return state;

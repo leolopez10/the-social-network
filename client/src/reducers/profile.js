3909c098d8b9d799f9a161c57e3558835cc13a0b
lil L <57925562+leolopez10@users.noreply.github.com>
2020-05-10 19:24:33 -0500

    got developers link to show profiles

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,9 +1,67 @@
+// import {
+//   GET_PROFILE,
+//   GET_PROFILES,
+//   PROFILE_ERROR,
+//   CLEAR_PROFILE,
+//   UPDATE_PROFILE,
+//   GET_REPOS
+// } from '../actions/types';
+
+// const initialState = {
+//   profile: null,
+//   profiles: [],
+//   repos: [],
+//   loading: true,
+//   error: {}
+// };
+
+// export default (state = initialState, action) => {
+//   const { type, payload } = action;
+
+//   switch (type) {
+//     case GET_PROFILE:
+//     case UPDATE_PROFILE:
+//       return {
+//         ...state,
+//         profile: payload,
+//         loading: false
+//       };
+//     case GET_PROFILES:
+//       return {
+//         ...state,
+//         profiles: payload,
+//         loading: false
+//       };
+//     case PROFILE_ERROR:
+//       return {
+//         ...state,
+//         error: payload,
+//         loading: false
+//       };
+//     case CLEAR_PROFILE:
+//       return {
+//         ...state,
+//         profiles: null,
+//         repos: [],
+//         loading: false
+//       };
+//     case GET_REPOS:
+//       return {
+//         ...state,
+//         repos: payload,
+//         loading: false
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
 import {
   GET_PROFILE,
-  GET_PROFILES,
   PROFILE_ERROR,
   CLEAR_PROFILE,
   UPDATE_PROFILE,
+  GET_PROFILES,
   GET_REPOS
 } from '../actions/types';
 
@@ -15,7 +73,7 @@ const initialState = {
   error: {}
 };
 
-export default (state = initialState, action) => {
+export default function (state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
@@ -36,12 +94,13 @@ export default (state = initialState, action) => {
       return {
         ...state,
         error: payload,
-        loading: false
+        loading: false,
+        profile: null
       };
     case CLEAR_PROFILE:
       return {
         ...state,
-        profiles: null,
+        profile: null,
         repos: [],
         loading: false
       };
@@ -54,4 +113,4 @@ export default (state = initialState, action) => {
     default:
       return state;
   }
-};
+}

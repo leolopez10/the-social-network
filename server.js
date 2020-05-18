1d917070b273c536a41a8244262823e8670f6a22
lil L <57925562+leolopez10@users.noreply.github.com>
2020-05-18 12:52:48 -0500

    trying to fix server file for development again

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express).urlencoded({ extended: true });
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
+// app.get('/', (req, res) => res.send('API Running'));
 
 // Serve up static assets (for heroku)
 if (process.env.NODE_ENV === 'production') {

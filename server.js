6da015c0582702272531b2222e14a473dd5f86ae
lil L <57925562+leolopez10@users.noreply.github.com>
2020-05-18 13:51:18 -0500

    blah

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Init Middleware
-app.use(express).urlencoded({ extended: true });
 app.use(express.json({ extended: false }));
 
 // Here for develpment

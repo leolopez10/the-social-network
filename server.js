7d7e7f1d1a71b7cf01031a0b35b298aae227093e
lil L <57925562+leolopez10@users.noreply.github.com>
2020-05-18 13:01:42 -0500

    deploy damn you!!

--- server.js
+++ server.js
@@ -15,15 +15,15 @@ app.use(express.json({ extended: false }));
 // Serve up static assets (for heroku)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+  // Send every other request to the React app
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+  });
 }
 
 // API Routes
 app.use(routes);
-
-// Send every other request to the React app
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
 
 //Database Connection
 mongoose
